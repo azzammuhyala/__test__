706d35b9946dc49590c0bfedab0b7a56e8dd8413
azzammuhyala <143919179+azzammuhyala@users.noreply.github.com>
2026-08-11 20:50:28 +0700

    Update kembang api.py

--- kembang api.py
+++ kembang api.py
@@ -1,13 +1,16 @@
 # @azzammuhyala - Bro, 
 
+# ini cuma cara bikin animasi percikan aja..
+
 import turtle as t
 import time as tm
 
 def start():
+    # bikin garis ke atas
     t.penup()
     t.speed(2)
     t.pencolor('green')
-    t.pensize(10)
+    t.pensize(3)
     t.setheading(90)
     t.goto(0, -100)
     t.pendown()
@@ -16,19 +19,21 @@ def start():
 
 def boom():
     t.pencolor('red')
-    # 2 method ini penting biar penebaran apinya natural
+    # 2 method ini penting biar penyebaran percikannya natural
     t.tracer(0)
     t.speed(0)
-    n = 20
-    for i in range(1, 21):
+    n = 30
+    for i in range(1, 16):
         for j in range(n):
             t.penup()
+            # titik awal percikan
             t.goto(0, 100)
+
             t.setheading(360/n * j)
             t.pendown()
             t.forward(i * 10)
         tm.sleep(0.016) # kasih delay biar keliatan animasinya (60 FPS)
-        t.update() # perbarui kanva (kayaknya)
+        t.update()      # perbarui kanva
 
 start()
 boom()
